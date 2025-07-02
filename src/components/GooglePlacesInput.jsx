0e9514dd7371f307132f6634725f011f3d93917b
BebeClick Development Team <bebeclick.dev@gmail.com>
2025-07-02 15:25:00 +0200

    Fix: Add loading=async to Google Maps API and update API key configuration

--- src/components/GooglePlacesInput.jsx
+++ src/components/GooglePlacesInput.jsx
@@ -46,7 +46,7 @@ const GooglePlacesInput = ({
     }
 
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places&language=fr`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places&language=fr&loading=async`;
     script.async = true;
     script.defer = true;
     
